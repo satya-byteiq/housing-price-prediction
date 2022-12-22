01a22b08b2cea820ba76f43c9aa476e8e928a0ab
satyapati <iamsatyapati@gmail.com>
2022-12-23 00:31:54 +0530

    downloading the dataset from kaggle

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,7 +1,93 @@
-import sys
+import os
+from zipfile import ZipFile
 from dotenv import load_dotenv
+import yaml
 
-load_dotenv()
 
-def download_dataset():
-    pass
+def load_env_variables():
+    """
+    Load the environment variable
+    :return: None
+    """
+    load_dotenv()
+    print("env variable loaded successfully!")
+
+
+def read_yml_file(path_name, key='dataset'):
+    """
+    Will read the dataset_info.yml file and return the info about dataset
+    :return:
+    """
+    with open(path_name, "r") as stream:
+        try:
+            dataset_info = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return dataset_info[key]
+
+
+def download_dataset(dataset_info):
+    """
+    Download the dataset into a directory
+    :param dataset_info: dataset info dictionary
+    :return: None
+    """
+    raw_data_dir = dataset_info['data-directory-path']
+    download_command = f"{dataset_info['download-command']} -c {dataset_info['name']} -p " \
+                       f"{raw_data_dir}"
+    if not os.path.exists(raw_data_dir):
+        print("directory absence! creating directory...")
+        os.mkdir(raw_data_dir)
+    try:
+        os.system(download_command)
+        print("downloading started!")
+    except OSError as e:
+        print(e.args)
+
+
+def unzip_file(path, file_name, type_file='zip'):
+    """
+    To unzip the given file into the current direction
+    :param path: the given path to the zip file
+    :param file_name: zip file name
+    :param type_file: type  of zip file
+    :return: None
+    """
+    try:
+        print("unzipping started!")
+        with ZipFile(os.path.join(path, file_name), 'r') as zObject:
+            zObject.extractall(path=path)
+        zObject.close()
+    except OSError as e:
+        print(e.args)
+
+
+def main():
+    """
+    Perform certain steps to have all the dataset files. Steps are: download, unzip
+    and removing the zip file
+
+    :return: None
+    """
+    load_env_variables()
+    dataset_config_path = "../config/dataset_info.yml"
+    dataset_info = read_yml_file(dataset_config_path)
+    # download the dataset to the given directory
+    download_dataset(dataset_info)
+    name_of_dataset = dataset_info['name']
+    target_path = dataset_info['data-directory-path']
+    ls_files = [i for i in os.listdir(target_path) if name_of_dataset in i]
+    zip_file = None
+    if len(ls_files) != 0:
+        print("dataset downloaded!")
+        extension = ls_files[0].split(os.path.sep)[-1]
+        unzip_file(target_path, ls_files[0], extension)
+        print("Zip file extracted!")
+        zip_file = [i for i in ls_files if name_of_dataset in i and extension in i][0]
+    ls_files = os.listdir(target_path)
+    if len(ls_files) > 1:
+        os.remove(os.path.join(target_path, zip_file))
+
+
+if __name__ == "__main__":
+    main()
